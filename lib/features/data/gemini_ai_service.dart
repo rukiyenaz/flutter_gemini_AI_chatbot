9f2f47d5b4807769710a6060f89520145879959e
rukiyenaz <rukiyenazyazgan51@gmail.com>
2025-05-18 14:52:13 +0300

    promt eklendi

--- lib/features/data/gemini_ai_service.dart
+++ lib/features/data/gemini_ai_service.dart
@@ -2,13 +2,12 @@ import 'dart:convert';
 
 import 'package:flutter_application_1/features/domain/entities/message.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:http/http.dart' as http;
 
 class GeminiAIService {
   
 
-  final String apiKey = 'AIzaSyBxoNMLS1p-J0Va-yjvkcnGHUpda9U2CEQ'; // .env dosyasına taşınabilir
+  final String apiKey = dotenv.env['GEMINI_API_KEY']!; // .env dosyasına taşınabilir
   final String model = 'gemini-1.5-flash'; // veya gemini-pro vs
 
   Future<Message?> getGeminiResponse(String prompt) async {
@@ -20,6 +19,7 @@ class GeminiAIService {
       "contents": [
         {
           "parts": [
+            {"text": "Sen bir doktor asistanısın. Kullanıcının sorularına Türkçe ve sağlıklı açısından doğru, sade ve kibar cevaplar ver. Her cevabın sonuna 'mutlaka bir doktora danışmalısınız' ekle."},
             {"text": prompt}
           ]
         }
